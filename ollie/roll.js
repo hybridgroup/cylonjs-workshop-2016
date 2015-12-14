75253f06bc17f21fe58f68152b757019a12a6de9
deadprogram <ron@hybridgroup.com>
2015-12-14 11:40:11 -0800

    Add Ollie tutorials

---
+++ ollie/roll.js
@@ -0,0 +1,31 @@
+var Cylon = require("cylon");
+
+Cylon.robot({
+  connections: {
+    bluetooth: { adaptor: "central", uuid: "cc360e85785e", module: "cylon-ble"}
+  },
+
+  devices: {
+    ollie: { driver: "ollie"}
+  },
+
+  work: function(my) {
+    my.ollie.setRGB(0x00FFFF);
+
+    after(500, function() {
+      my.ollie.setRGB(0xFF0000);
+    });
+
+    after(1000, function() {
+      my.ollie.roll(60, 0, 1);
+    });
+
+    after(2000, function() {
+      my.ollie.roll(60, 180, 1);
+    });
+
+    after(3000, function() {
+      my.ollie.stop();
+    });
+  }
+}).start();
---
+++ ollie/spin.js
@@ -0,0 +1,27 @@
+var Cylon = require("cylon");
+
+Cylon.robot({
+  connections: {
+    bluetooth: { adaptor: "central", uuid: "cc361e85785e", module: "cylon-ble"}
+  },
+
+  devices: {
+    ollie: { driver: "ollie"}
+  },
+
+  work: function(my) {
+    my.ollie.setRGB(0xFF0000);
+    after((1).second(), function() {
+      my.ollie.spin("left", 200);
+    });
+    after((5).seconds(), function() {
+      my.ollie.spin("right", 200);
+    });
+    after((10).seconds(), function() {
+      my.ollie.stop();
+    });
+    after((12).seconds(), function() {
+      my.ollie.setRGB(0x0000FF);
+    });
+  }
+}).start();
